347050cac798d33a8323ef4725cc66df147fdce2
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-20 19:30:08 +0530

    Create Operations.cpp

---
+++ Operations.cpp
@@ -0,0 +1,25 @@
+// In c++ there are different Operations used-
+
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+int a = 3;
+int b = 1;
+// adding two number
+  cout<<"sum of two number a and b is "<<(a+b)<<endl;
+
+// sub. of teo number
+  cout<<"the sub. of two number is "<<(a-b)<<endl;
+  
+//  mul. of two number
+  cout<<"the mul. of two number is "<<(a*b)<<endl;
+  
+// divide of two number
+  cout<<"the divide of two number is "<<(a/b)<<endl;
+  
+// mod.of two number
+  cout<<"the mod. of two number is "<<(a%b)<<endl;
+
+}

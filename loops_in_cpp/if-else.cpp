0cd0b59e3bf0f45ec36dbcd74752a24633890f6a
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-21 22:35:53 +0530

    Create if-else.cpp

---
+++ loops_in_cpp/if-else.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int x = 20;
+int y = 10;
+  if(y>x)
+  {
+    cout<<"the statement is false"<<endl; 
+  }
+   if-else(y<x)
+   {
+     cout<<"the statement is true"<<endl;
+   }
+  else(x==y)
+  {
+     cout<<"the statement is false"<<endl;
+  }
+};

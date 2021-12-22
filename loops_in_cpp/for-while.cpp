cee157f93667f49e629d6d673595c2268c1c4013
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-22 21:03:25 +0530

    Create for-while.cpp

---
+++ loops_in_cpp/for-while.cpp
@@ -0,0 +1,18 @@
+// There are 3 types of loops in C++.
+
+// for loop
+// while loop
+// do-while loop
+
+do 
+{
+  // code block to be executed
+}
+while (condition);
+
+int i = 0;
+do {
+  cout << i << "\n";
+  i++;
+}
+while (i < 5);

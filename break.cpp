63068fe9dd9920efc3f5d9795d1b341f235fcbe4
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-23 22:35:27 +0530

    Create break.cpp

---
+++ break.cpp
@@ -0,0 +1,8 @@
+// break is type of condition from where the i will come-out from the loop from thet point.
+
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    break;
+  }
+  cout << i << "\n";
+}

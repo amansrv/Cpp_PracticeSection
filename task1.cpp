d8f08cd251361199f0fcb277333cf3d39d7fe0cd
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2021-12-30 22:04:59 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a;
+int b;
+
+cout<<"enter the value of a and b: "<<endl;
+cin>>a,b<<endl;
+cout<<"add both the number<<(a+b)<<endl;
+
+return 0;
+}

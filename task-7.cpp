93a29e907bd8c2d42977b7a3e3c01df06a68a9b1
Aman Srivastava <89743268+amansrv@users.noreply.github.com>
2022-02-05 22:35:31 +0530

    Create task-7.cpp

---
+++ task-7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void PrintArray(int arr[], int size) {
+  for (int i = 0; i <= 10; i++){
+  }
+}
+
+int main(){
+
+  int arr[15] = {1};
+  n=10;
+  PrintArray(arr,10)
+    return 0;
+  
+}
